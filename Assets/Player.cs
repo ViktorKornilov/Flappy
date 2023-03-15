2624cc058a9acb1db46468d283afe084d37f642b
Vik <vik :)>
2023-03-15 17:47:36 +0200

    Improved jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            print("jump");
+            transform.position += Vector3.up;
         }
     }
 }
